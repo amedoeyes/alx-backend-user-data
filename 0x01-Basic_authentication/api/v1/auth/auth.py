9f0f30c55daa5f7a206ee112ec65faa1158ba818
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-09-04 04:25:08 +0300

    add task 13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,12 +14,18 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication"""
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        if path is None or not excluded_paths:
             return True
-        if path[-1] != "/":
-            path += "/"
-        if path in excluded_paths:
-            return False
+        path = path + "/" if path[-1] != "/" else path
+        has_wildcard = any(x.endswith("*") for x in excluded_paths)
+        if not has_wildcard:
+            return path not in excluded_paths
+        for entry in excluded_paths:
+            if entry.endswith("*"):
+                if path.startswith(entry[:-1]):
+                    return False
+            if path == entry:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
