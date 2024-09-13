adb86ccef33535b52b35de91eea8f6fdc2d744ff
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-09-13 22:56:36 +0300

    add task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -90,3 +90,15 @@ class Auth:
         except Exception:
             return None
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Get reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
+        except Exception:
+            raise ValueError
