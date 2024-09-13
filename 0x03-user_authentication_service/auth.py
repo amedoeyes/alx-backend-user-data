42bcd13f1dafe375d88bf0de82e68a40b4c15a59
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-09-13 22:45:19 +0300

    add task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,3 +82,11 @@ class Auth:
         except Exception:
             return None
         return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy session"""
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except Exception:
+            return None
+        return None
