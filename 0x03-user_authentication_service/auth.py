e2d596b667fb93233aa1c6c4056a0bca3d095db4
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-09-13 22:34:05 +0300

    add task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,3 +29,18 @@ class Auth:
             raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate login"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+        except ValueError:
+            return False
+        except TypeError:
+            return False
+        except Exception:
+            return False
+        return False
